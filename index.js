f6fc8e47a31e4af96c1ca2205e3ae9ccda506965
VaskillerDev <Vaskoooo9241@gmail.com>
2020-11-15 01:16:49 +0300

    fmt

--- index.js
+++ index.js
@@ -9,98 +9,122 @@
 // -> find dir with content in /exampleDir
 // -> normalize with current rules
 // -> recursively add all files to example.mgcb
-const path = require("path");
-const fs = require("fs");
+const path = require('path');
+const fs = require('fs');
 const configFile = fs.readFileSync('./mgcb-gen-config.json');
 const config = JSON.parse(configFile.toString('utf-8'));
 
-console.log("Your config: ", config)
+console.log('Your config: ', config);
 
 const DEBUG = process.env.debug || config['debug'] || false;
-const ASEPRITE_LIB_PATH = process.env.asepriteLib || config['asepriteLib'] || "";
-const ASEPRITE_PIPELINE_PATH = process.env.asepritePipeline || config['asepritePipeline'] || "";
-const maybePathToContent = process.env.content  || config['content'] || new Error("Path to content directory not found.");
+const ASEPRITE_LIB_PATH = process.env.asepriteLib || config['asepriteLib'];
+const ASEPRITE_PIPELINE_PATH =
+  process.env.asepritePipeline || config['asepritePipeline'];
+const maybePathToContent =
+  process.env.content ||
+  config['content'] ||
+  new Error('Path to content directory not found.');
 
-// todo: add support aseprite lib and pipeline file check
-
-if (maybePathToContent instanceof Error) { // path not set
-    console.error(maybePathToContent);
-    process.exit(1);
+if (maybePathToContent instanceof Error) {
+  // path not set
+  console.error(maybePathToContent);
+  process.exit(1);
 }
 
 const purePathToContent = maybePathToContent; // content dir
-const pathToMGCB = path.join(purePathToContent,'content.mgcb');
+const pathToMGCB = path.join(purePathToContent, 'content.mgcb');
 
-if (!fs.existsSync(purePathToContent)) { // content dir not exist
-    console.error(new Error(`Content directory not exist: ${purePathToContent}`));
-    process.exit(1);
+if (!fs.existsSync(purePathToContent)) {
+  // content dir not exist
+  console.error(new Error(`Content directory not exist: ${purePathToContent}`));
+  process.exit(1);
 }
 // -- declaration
-const addAsepriteEntity = (mgcb,dirName) => { // require .json && .png file
-   try {
-       const pathToEntity = path.join(purePathToContent,dirName);
-       const list = fs.readdirSync(pathToEntity);
-       const spriteFile = list.filter(file=>path.extname(file) === ".png")[0];
-       const animationFile = list.filter(file=>path.extname(file) === ".json")[0];
+const addAsepriteEntity = (mgcb, dirName) => {
+  // require .json && .png file
+  try {
+    const pathToEntity = path.join(purePathToContent, dirName);
+    const list = fs.readdirSync(pathToEntity);
+    const spriteFile = list.filter(file => path.extname(file) === '.png')[0];
+    const animationFile = list.filter(
+      file => path.extname(file) === '.json'
+    )[0];
 
-       if (DEBUG) console.log(`dirName: ${dirName} \nSprite file: ${spriteFile} \nAnimation file: ${animationFile}`);
-       checkFile(spriteFile,`Sprite file not found in ${dirName}`);
-       checkFile(animationFile,`Animation file not found in ${dirName}`);
+    if (DEBUG)
+      console.log(
+        `dirName: ${dirName} \nSprite file: ${spriteFile} \nAnimation file: ${animationFile}`
+      );
+    checkFile(spriteFile, `Sprite file not found in ${dirName}`);
+    checkFile(animationFile, `Animation file not found in ${dirName}`);
 
-       // .json
-       fs.appendFileSync(mgcb,`#begin ${dirName}/${animationFile}\n`);
-       fs.appendFileSync(mgcb,"/importer:AsepriteImporter\n" +
-           "/processor:Processor\n");
-       fs.appendFileSync(mgcb,`/build:${dirName}/${animationFile}\n\n`);
+    // .json
+    fs.appendFileSync(mgcb, `#begin ${dirName}/${animationFile}\n`);
+    fs.appendFileSync(
+      mgcb,
+      '/importer:AsepriteImporter\n' + '/processor:Processor\n'
+    );
+    fs.appendFileSync(mgcb, `/build:${dirName}/${animationFile}\n\n`);
 
-       // .png
-       fs.appendFileSync(mgcb, `#begin ${dirName}/${spriteFile}\n`);
-       fs.appendFileSync(mgcb,"/importer:TextureImporter\n" +
-           "/processor:TextureProcessor\n" +
-           "/processorParam:ColorKeyColor=255,0,255,255\n" +
-           "/processorParam:ColorKeyEnabled=True\n" +
-           "/processorParam:GenerateMipmaps=False\n" +
-           "/processorParam:PremultiplyAlpha=True\n" +
-           "/processorParam:ResizeToPowerOfTwo=False\n" +
-           "/processorParam:MakeSquare=False\n" +
-           "/processorParam:TextureFormat=Color\n");
-       fs.appendFileSync(mgcb,`/build:${dirName}/${spriteFile}\n\n`);
-
-   } catch (e) {
-    console.error("Error in addAsepriteEntity: "+ e);
-   }
-}
-const addAsepriteReferences = (mgcb) => { // todo: hardcoded
-    if (!!DEBUG) {
-    console.log("asepriteLib: " + ASEPRITE_LIB_PATH);
-    console.log("asepritePipeline: " + ASEPRITE_PIPELINE_PATH);
-    }
-    fs.appendFileSync(mgcb,`/reference:${ASEPRITE_LIB_PATH}\n`);
-    fs.appendFileSync(mgcb,`/reference:${ASEPRITE_PIPELINE_PATH}\n`);
-}
-const checkFile = (file,msg) => {
-    if (!file) throw new Error(msg);
-}
-const addWindowsConfig = (mgcb) => {
-    fs.appendFileSync(mgcb,"/outputDir:bin\n" +
-        "/intermediateDir:obj\n" +
-        "/platform:Windows\n" +
-        "/config:\n" +
-        "/profile:Reach\n" +
-        "/compress:False\n")
-}
+    // .png
+    fs.appendFileSync(mgcb, `#begin ${dirName}/${spriteFile}\n`);
+    fs.appendFileSync(
+      mgcb,
+      '/importer:TextureImporter\n' +
+        '/processor:TextureProcessor\n' +
+        '/processorParam:ColorKeyColor=255,0,255,255\n' +
+        '/processorParam:ColorKeyEnabled=True\n' +
+        '/processorParam:GenerateMipmaps=False\n' +
+        '/processorParam:PremultiplyAlpha=True\n' +
+        '/processorParam:ResizeToPowerOfTwo=False\n' +
+        '/processorParam:MakeSquare=False\n' +
+        '/processorParam:TextureFormat=Color\n'
+    );
+    fs.appendFileSync(mgcb, `/build:${dirName}/${spriteFile}\n\n`);
+  } catch (e) {
+    console.error('Error in addAsepriteEntity: ' + e);
+  }
+};
+const addAsepriteReferences = mgcb => {
+  // todo: hardcoded
+  if (DEBUG) {
+    console.log('asepriteLib: ' + ASEPRITE_LIB_PATH);
+    console.log('asepritePipeline: ' + ASEPRITE_PIPELINE_PATH);
+  }
+  fs.appendFileSync(mgcb, `/reference:${ASEPRITE_LIB_PATH}\n`);
+  fs.appendFileSync(mgcb, `/reference:${ASEPRITE_PIPELINE_PATH}\n`);
+};
+const checkFile = (file, msg) => {
+  if (!file) throw new Error(msg);
+};
+const addWindowsConfig = mgcb => {
+  fs.appendFileSync(
+    mgcb,
+    '/outputDir:bin\n' +
+      '/intermediateDir:obj\n' +
+      '/platform:Windows\n' +
+      '/config:\n' +
+      '/profile:Reach\n' +
+      '/compress:False\n'
+  );
+};
 // -- start
-console.log(`Content path: ${purePathToContent}`)
-console.log(`MGCB path: ${pathToMGCB}`)
+console.log(`Content path: ${purePathToContent}`);
+console.log(`MGCB path: ${pathToMGCB}`);
 
 if (fs.existsSync(pathToMGCB)) fs.unlinkSync(pathToMGCB);
 
-fs.writeFileSync(pathToMGCB,"\n#----------------------------- Global Properties ----------------------------#\n\n");
+fs.writeFileSync(
+  pathToMGCB,
+  '\n#----------------------------- Global Properties ----------------------------#\n\n'
+);
 addWindowsConfig(pathToMGCB);
-fs.appendFileSync(pathToMGCB,"\n#-------------------------------- References --------------------------------#\n\n");
+fs.appendFileSync(
+  pathToMGCB,
+  '\n#-------------------------------- References --------------------------------#\n\n'
+);
 addAsepriteReferences(pathToMGCB);
-fs.appendFileSync(pathToMGCB,"\n#---------------------------------- Content ---------------------------------#\n\n");
-addAsepriteEntity(pathToMGCB,"human");
-
-
-
+fs.appendFileSync(
+  pathToMGCB,
+  '\n#---------------------------------- Content ---------------------------------#\n\n'
+);
+addAsepriteEntity(pathToMGCB, 'human');
