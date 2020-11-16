fec28df23784401541a24692ff13cbe5bc609a6c
VaskillerDev <Vaskoooo9241@gmail.com>
2020-11-16 04:09:59 +0300

    fmt
    added auto search support Aseprite nuget packages, wsl support fix

--- init.js
+++ init.js
@@ -2,43 +2,44 @@
 const fs = require('fs');
 const path = require('path');
 const config = require('./mgcb-gen-config-template.json');
+const wsl = require('./wslPath.js');
 const pathToMgcbGenConfigFile = './mgcb-gen-config.json';
-const configString = JSON.stringify(config, null, ' ');
 
+// --declaration
 const NOTICE_MSG = 'mgcb_init: [Notice]';
 const WARN_MSG = 'mgcb_init: [Warn]';
 
-const search = (source,target) => {
+const search = (source, target) => {
   // --declaration
-  const searchSync = (collector,source,target) => { // recursive search
-    const files = fs.readdirSync(source,{withFileTypes: true});
+  const searchSync = (collector, source, target) => {
+    // recursive search
+    const files = fs.readdirSync(source, { withFileTypes: true });
     const dirs = files.filter(file => file.isDirectory()).map(dir => dir.name);
     if (files.length === 0 || !files) return;
     files.forEach(file => {
       if (file.name === target) {
-        const targetPath = path.join(source,file.name);
+        const targetPath = path.join(source, file.name);
         collector.push(targetPath);
       }
     });
     if (dirs.length === 0 || !dirs) return;
     for (const dir of dirs) {
-      const tmpPath = path.resolve(source,dir.toString());
-      searchSync(collector,tmpPath,target);
+      const tmpPath = path.resolve(source, dir.toString());
+      searchSync(collector, tmpPath, target);
     }
-  }
+  };
 
   // --impl
   const collector = [];
-  searchSync(collector,source,target);
+  searchSync(collector, source, target);
   return collector;
-}
+};
 
+// --impl
 if (fs.existsSync(pathToMgcbGenConfigFile)) {
   console.warn(`${NOTICE_MSG} ${pathToMgcbGenConfigFile} exist`);
   process.exit(0);
 }
-
-fs.writeFileSync(pathToMgcbGenConfigFile, configString);
 
 {
   // simple search MonoGame.Aseprite.dll && MonoGame.Aseprite.ContentPipeline.dll
@@ -47,13 +48,15 @@ fs.writeFileSync(pathToMgcbGenConfigFile, configString);
   const last = dirs.length - 1;
   const candidatePaths = [];
   const asepriteNameLike = new RegExp('[Aa]seprite');
+  const netstandardNameLike = new RegExp('[Nn]etstandard');
 
   let slide = last;
   let currentPath = path.resolve(dirs[last]);
   let nugetDir;
   while (slide > 0) {
     currentPath = path.resolve(currentPath, '../');
-    fs.readdirSync(currentPath).find(file => { // search nuget-like dirs
+    fs.readdirSync(currentPath).find(file => {
+      // search nuget-like dirs
       // require dirs
       let isNugetDir = false;
       if (file === '.nuget') (nugetDir = path.join(currentPath, file, 'packages')) && (isNugetDir = true); // for usr dir
@@ -66,24 +69,37 @@ fs.writeFileSync(pathToMgcbGenConfigFile, configString);
     console.warn(`${WARN_MSG} nuget packages dir not found. Please set the path manually.`);
   } else {
     const candidateAsepritePaths = [];
-    candidatePaths.forEach(candidatePath => { // search asprite-like dirs
+    candidatePaths.forEach(candidatePath => {
+      // search asprite-like dirs
       const files = fs.readdirSync(candidatePath);
       const asepriteLib = files.filter(file => asepriteNameLike.test(file)).toString();
       if (asepriteLib) candidateAsepritePaths.push(path.join(candidatePath, asepriteLib));
     });
     if (candidateAsepritePaths.length !== 0) {
-      let collectionPotentialWithPipelineLib =
-      candidateAsepritePaths.map(candidatePath =>  // search aseprite files
-        search(candidatePath,"MonoGame.Aseprite.ContentPipeline.dll")
-      ).flat();
-      let collectionPotentialWithLib =
-          candidateAsepritePaths.map(candidatePath =>  // search aseprite files
-              search(candidatePath,"MonoGame.Aseprite.dll")
-          ).flat();
-      /*console.log(collectionPotentialWithPipelineLib);
-      console.log(collectionPotentialWithLib);*/
-      // todo: set found paths into config
+      let collectionPotentialWithPipelineLib = candidateAsepritePaths
+        .map((
+          candidatePath // search aseprite files
+        ) => search(candidatePath, 'MonoGame.Aseprite.ContentPipeline.dll'))
+        .flat();
+      let collectionPotentialWithLib = candidateAsepritePaths
+        .map((
+          candidatePath // search aseprite files
+        ) => search(candidatePath, 'MonoGame.Aseprite.dll'))
+        .flat();
+
+      const endedPathToLib = collectionPotentialWithLib.filter(potentialPath =>
+        netstandardNameLike.test(potentialPath.toString())
+      )[0];
+      const endedPathToPipelineLib = collectionPotentialWithPipelineLib.filter(potentialPath =>
+        netstandardNameLike.test(potentialPath.toString())
+      )[0];
+      if (!endedPathToLib && !endedPathToPipelineLib) return; // todo: work with this line for way wit alternatives search
+
+      config.asepriteLib = wsl.wslToWindowsSync(endedPathToLib) || endedPathToLib;
+      config.asepritePipeline = wsl.wslToWindowsSync(endedPathToPipelineLib) || endedPathToPipelineLib;
+
+      const configString = JSON.stringify(config, null, ' ');
+      fs.writeFileSync(pathToMgcbGenConfigFile, configString);
     }
   }
 }
-
--- wslPath.js
+++ wslPath.js
@@ -1,21 +1,23 @@
-function isWslPath(wslPath) { // simple check
-    return wslPath.includes('/mnt/');
+function isWslPath(wslPath) {
+  // simple check
+  return wslPath.includes('/mnt/');
 }
 
-function unsafeWslToWindowsSync (wslPath) { // convert wsl path to windows
-    const parsedWslPath = wslPath.split('/');
-    const usersIndex = parsedWslPath.indexOf('Users');
-    const winVD = parsedWslPath[usersIndex - 1].toUpperCase() + ":/"; // get virtual disk name
-    const winUserPath =  parsedWslPath.slice(usersIndex,parsedWslPath.length - 1);
-    return winVD + winUserPath.join('/');
+function unsafeWslToWindowsSync(wslPath) {
+  // convert wsl path to windows
+  const parsedWslPath = wslPath.split('/');
+  const usersIndex = parsedWslPath.indexOf('Users');
+  const winVD = parsedWslPath[usersIndex - 1].toUpperCase() + ':/'; // get virtual disk name
+  const winUserPath = parsedWslPath.slice(usersIndex, parsedWslPath.length - 1);
+  return winVD + winUserPath.join('/');
 }
 
 function wslToWindowsSync(wslPath) {
-    if (isWslPath(wslPath)) return unsafeWslToWindowsSync(wslPath);
-    return undefined;
+  if (isWslPath(wslPath)) return unsafeWslToWindowsSync(wslPath);
+  return undefined;
 }
 
 module.exports = {
-    isWslPath: isWslPath,
-    wslToWindowsSync: wslToWindowsSync,
-}
+  isWslPath: isWslPath,
+  wslToWindowsSync: wslToWindowsSync,
+};
