15d63e11ce8bb67190f687cceb97eddbb9a7a512
VaskillerDev <Vaskoooo9241@gmail.com>
2020-11-16 15:58:38 +0300

    init cli-support

---
+++ mgcbg.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+const command = process.argv[2] || showHelp();
+
+function showHelp() {
+  console.log(
+    '\n' +
+      'Usage: mgcbg <command> <flags> \n\n' +
+      'where is one of: \n' +
+      'init, gen\n\n' +
+      'mgcbg help         quick help\n' +
+      'mgcbg init         init mgcb-gen-config.json file for generate .mgcb file content\n' +
+      'mgcbg gen          genearte .mgcb file content\n'
+  );
+  return null;
+}
+if (command) {
+  switch (command) {
+    case 'init':
+      require('./init.js');
+      break;
+    case 'gen':
+      require('./gen.js');
+      break;
+    default:
+      showHelp();
+  }
+}
