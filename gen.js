7cf302f4cd264704e958c5c32c0f271b061cfa90
VaskillerDev <Vaskoooo9241@gmail.com>
2020-11-29 08:13:04 +0300

    add execute mgcb feature, +execEditor(), modify addWindowsConfig(),addAndroidConfig()

--- gen.js
+++ gen.js
@@ -5,12 +5,15 @@
 //      debug: boolean,
 //      asepriteLib: string,
 //      asepritePipeline: string,
+//      extendedContentPipeline: string,
+//      editor: string
 // -- principle of operation:
 // -> find dir with content in /exampleDir
 // -> normalize with current rules
 // -> recursively add all files to example.mgcb
 const path = require('path');
 const fs = require('fs');
+const wsl = require("./wslPath");
 
 let configFile;
 let config;
@@ -27,6 +30,7 @@ const DEBUG = process.env.debug || config['debug'] || false;
 const ASEPRITE_LIB_PATH = process.env.asepriteLib || config['asepriteLib'];
 const ASEPRITE_PIPELINE_PATH = process.env.asepritePipeline || config['asepritePipeline'];
 const EXTENDED_CONTENT_PIPELINE_PATH = process.env.extendedContentPipeline || config['extendedContentPipeline'];
+const EDITOR_PATH = process.env.editor || config['editor'];
 const maybePathToContent =
   process.env.content || config['content'] || new Error('Path to content directory not found.');
 
@@ -156,8 +160,8 @@ function checkFile(file, msg) {
 function addWindowsConfig(mgcb) {
   fs.appendFileSync(
     mgcb,
-    '/outputDir:bin\n' +
-      '/intermediateDir:obj\n' +
+    '/outputDir:win_bin\n' +
+      '/intermediateDir:win_obj\n' +
       '/platform:Windows\n' +
       '/config:\n' +
       '/profile:Reach\n' +
@@ -168,13 +172,35 @@ function addWindowsConfig(mgcb) {
 function addAndroidConfig(mgcb) {
   fs.appendFileSync(
     mgcb,
-    '/outputDir:bin\n' +
-      '/intermediateDir:obj\n' +
+    '/outputDir:android_bin\n' +
+      '/intermediateDir:android_obj\n' +
       '/platform:Android\n' +
       '/config:\n' +
       '/profile:Reach\n' +
       '/compress:False\n'
   );
+}
+
+function execEditor() {
+  const cp = require('child_process');
+  const os = require('os');
+  let editorPath = wsl.toWindowsSync(EDITOR_PATH) || EDITOR_PATH;
+
+  if (os.platform() === "linux") editorPath = wsl.toWslSync(editorPath);
+
+  const buildWithMgcbEditor =  (mgcbFile) => {
+    const command = ` ${editorPath} /b ${mgcbFile}`;
+    console.log("Mgcb run: " + command);
+    try {
+      cp.execSync(command,{cwd: config['content']});
+    } catch (out) {
+      if (out.stderr.length > 0) console.error(out.stderr.toString());
+      if (out.stdout.length > 0) console.log(out.stdout.toString());
+    }
+
+  }
+   buildWithMgcbEditor('win_content.mgcb');
+   buildWithMgcbEditor('android_content.mgcb');
 }
 
 function createMgcbFile(prefix, pathToContent, addConfig) {
@@ -185,14 +211,20 @@ function createMgcbFile(prefix, pathToContent, addConfig) {
   fs.writeFileSync(pathToMGCB, '\n#----------------------------- Global Properties ----------------------------#\n\n');
   addConfig(pathToMGCB);
   fs.appendFileSync(pathToMGCB, '\n#-------------------------------- References --------------------------------#\n\n');
-  addAsepriteReferences(pathToMGCB);
-  addMonogameExtendedcContentPipeline(pathToMGCB);
+  if (ASEPRITE_LIB_PATH && ASEPRITE_PIPELINE_PATH) addAsepriteReferences(pathToMGCB);
+  if (EXTENDED_CONTENT_PIPELINE_PATH) addMonogameExtendedcContentPipeline(pathToMGCB);
   fs.appendFileSync(pathToMGCB, '\n#---------------------------------- Content ---------------------------------#\n\n');
   const dirs = fs
     .readdirSync(pathToContent, { withFileTypes: true })
     .filter(file => file.isDirectory())
     .map(dir => dir.name)
-    .filter(dirName => dirName.toString() !== 'obj' && dirName.toString() !== 'bin');
+    .filter(dirName => dirName.toString() !== 'obj'
+        && dirName.toString() !== 'bin'
+        && dirName.toString() !== 'win_bin'
+        && dirName.toString() !== 'win_obj'
+        && dirName.toString() !== 'android_bin'
+        && dirName.toString() !== 'android_obj'
+    );
 
   for (const dir of dirs) {
     addAsepriteEntity(pathToMGCB, dir) || addTiledEntity(pathToMGCB, dir);
@@ -202,3 +234,4 @@ function createMgcbFile(prefix, pathToContent, addConfig) {
 // -- start
 createMgcbFile('win', purePathToContent, addWindowsConfig);
 createMgcbFile('android', purePathToContent, addAndroidConfig);
+execEditor(purePathToContent);
