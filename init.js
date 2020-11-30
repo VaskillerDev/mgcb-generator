770a5178fd906e5e24dfe407dbcbeccfca118a5c
VaskillerDev <Vaskoooo9241@gmail.com>
2020-11-30 07:01:03 +0300

    minor naming fix, added support simple search mgcb.exe

--- init.js
+++ init.js
@@ -43,6 +43,11 @@ if (fs.existsSync(pathToMgcbGenConfigFile)) {
   console.warn(`${NOTICE_MSG} ${pathToMgcbGenConfigFile} exist`);
   process.exit(0);
 }
+/*
+ * the search files are very similar,
+ * but they may require an individual approach,
+ * so should separate the implementations just in case
+ */
 
 {
   // simple search MonoGame.Aseprite.dll && MonoGame.Aseprite.ContentPipeline.dll
@@ -134,18 +139,18 @@ if (fs.existsSync(pathToMgcbGenConfigFile)) {
   if (nugetDirs.length === 0) {
     console.warn(`${WARN_MSG} nuget packages dir not found. Please set the path manually.`);
   } else {
-    const asepriteFiles = [];
+    const monogamePipelineFiles = [];
 
     nugetDirs.forEach(candidatePath => {
-      // search aseprite-like dirs
+      // search pipeline-like dirs
       const files = fs.readdirSync(candidatePath);
-      const asepriteLib = files.filter(file => extendedContentPipelineRegex.test(file)).toString();
-      if (asepriteLib) asepriteFiles.push(path.join(candidatePath, asepriteLib));
+      const pipelineLib = files.filter(file => extendedContentPipelineRegex.test(file)).toString();
+      if (pipelineLib) monogamePipelineFiles.push(path.join(candidatePath, pipelineLib));
     });
 
-    if (asepriteFiles.length !== 0) {
-      let collectionPotentialWithPipelineLib = asepriteFiles
-          .map(candidatePath => search(candidatePath, 'MonoGame.Extended.Content.Pipeline.dll')) // search aseprite files
+    if (monogamePipelineFiles.length !== 0) {
+      let collectionPotentialWithPipelineLib = monogamePipelineFiles
+          .map(candidatePath => search(candidatePath, 'MonoGame.Extended.Content.Pipeline.dll')) // search monogame extended files
           .flat();
 
       const endedPathToPipelineLib = collectionPotentialWithPipelineLib.filter(potentialPath =>
@@ -155,6 +160,61 @@ if (fs.existsSync(pathToMgcbGenConfigFile)) {
       if (!endedPathToPipelineLib) return; // todo: work with this line for way wit alternatives search
 
       config.extendedContentPipeline = wsl.toWindowsSync(endedPathToPipelineLib) || endedPathToPipelineLib;
+
+      const configString = JSON.stringify(config, null, ' ');
+      fs.writeFileSync(pathToMgcbGenConfigFile, configString);
+    }
+  }
+}
+
+{
+  // simple search mgcb.exe (support by 3.8)
+  const dirs = path.resolve('./', './').split(path.sep);
+  const last = dirs.length - 1;
+  const nugetDirs = [];
+  const contentBuilderTaskRegex = new RegExp('[Mm]onogame\.content\.builder\.task');
+  const versionRegex = new RegExp('3\.8');
+
+  let slide = last;
+  let currentPath = path.resolve(dirs[last]);
+  let nugetDir;
+  while (slide > 0) {
+    currentPath = path.resolve(currentPath, '../');
+    fs.readdirSync(currentPath).find(file => {
+      // search nuget-like dirs
+      // require dirs
+      let isNugetDir = false;
+      if (file === '.nuget') (nugetDir = path.join(currentPath, file, 'packages')) && (isNugetDir = true); // for usr dir
+      if (file === 'packages') (nugetDir = path.join(currentPath, file)) && (isNugetDir = true);
+      return isNugetDir;
+    }) && nugetDirs.push(nugetDir); // side effect
+    --slide;
+  }
+
+  if (nugetDirs.length === 0) {
+    console.warn(`${WARN_MSG} nuget packages dir not found. Please set the path manually.`);
+  } else {
+    const mgcbToolFiles = [];
+
+    nugetDirs.forEach(candidatePath => {
+      // search mgcb-tool-like dirs
+      const files = fs.readdirSync(candidatePath);
+      const asepriteLib = files.filter(file => contentBuilderTaskRegex.test(file)).toString();
+      if (asepriteLib) mgcbToolFiles.push(path.join(candidatePath, asepriteLib));
+    });
+
+    if (mgcbToolFiles.length !== 0) {
+      let collectionPotentialTools = mgcbToolFiles
+          .map(candidatePath => search(candidatePath, 'mgcb.exe')) // search aseprite files
+          .flat();
+
+      const endedPathToTool= collectionPotentialTools.filter(potentialPath =>
+          versionRegex.test(potentialPath.toString())
+      )[0];
+
+      if (!endedPathToTool) return; // todo: work with this line for way wit alternatives search
+
+      config.editor = wsl.toWindowsSync(endedPathToTool) || endedPathToTool;
 
       const configString = JSON.stringify(config, null, ' ');
       fs.writeFileSync(pathToMgcbGenConfigFile, configString);
